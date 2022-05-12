7316f9fc7190c69ee115bab07aa679d830d7e7e1
Johan Isfiehan <104421166+johansofie@users.noreply.github.com>
2022-05-12 16:38:47 +0700

    Convert HTML to svelte components

---
+++ src/data/charities.js
@@ -0,0 +1,18 @@
+module.exports = {
+  charities: [
+    {
+      id: 1,
+      title: "First Charity Project",
+      category: "Money",
+      thumbnail:
+        "https://images.unsplash.com/photo-1469571486292-0ba58a3f068b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+      pledged: 90000,
+      target: 100000,
+      date_end: +new Date("22 May 2022"),
+      profile_photo:
+        "https://images.unsplash.com/photo-1591014441032-1c4302d2191b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80",
+      profile_name: "Serena Nusaibah Isfiehan",
+      no_pledges: 0,
+    },
+  ],
+};
---
+++ src/pages/Home.svelte
@@ -0,0 +1,20 @@
+<script>
+	import CharityList from '../components/CharityList.svelte';
+	import Header from '../components/Header.svelte'
+	import Welcome from '../components/Welcome.svelte'
+	import Promo from '../components/Promo.svelte'
+	import Footer from '../components/Footer.svelte'
+	import { charities } from  '../data/charities.js';
+	let title = "Charity";
+	
+
+</script>
+
+<style>
+</style>
+
+<Header/>
+<Welcome/>
+<CharityList {charities}/>
+<Promo/>
+<Footer/>
